70a8eefce55c22525b8f2b299276a18af01e5bf0
GoldFresh <valerazmey95@gmail.com>
2023-01-10 19:02:41 +0300

    fixed #1

--- bonus-service/src/main/java/Main.java
+++ bonus-service/src/main/java/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
+    long expected = 30;
 
-    service.calculate(amount, registered);
+    long actual = service.calculate(amount, registered);
+
+    boolean passed = actual == expected;
+
+    System.out.println(passed);
   }
 }
 
